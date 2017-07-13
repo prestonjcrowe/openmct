47bbd0b94aaf5178bfd31fd4c5127c870e1d7c84
Preston Crowe <prestonjcrowe@gmail.com>
2017-07-13 16:40:26 -0700

    [Import/Export] JSON read/write strcutre established

--- import-export/ExportAsJSONAction.js
+++ import-export/ExportAsJSONAction.js
@@ -10,18 +10,43 @@ define([], function () {
     ExportAsJSONAction.prototype.perform = function() {
         var context = this.context;
         var domainObject = this.context.domainObject;
-        this.exportService.exportJSON(domainObject.getModel(), 
-            {filename:  Date.now() + '.json'});
+        // this.exportService.exportJSON(domainObject.getModel(), 
+        //     {filename:  Date.now() + '.json'});
 
-        domainObject.useCapability('composition').then(function (children) {
-            children.forEach(function (child) {
-                console.log('child: ' + JSON.stringify(child.getModel()));
-            })
-        })
+        this.contructJSON(domainObject);
     }; 
 
-    ExportAsJSONAction.appliesTo = function (context) {
+    ExportAsJSONAction.prototype.appliesTo = function (context) {
         return context.domainObject !== undefined;
+    };
+
+    ExportAsJSONAction.prototype.contructJSON = function (rootObject) {
+        var objJSON = {};
+        objJSON["root"] = rootObject.getModel();
+        this.writeChildren(objJSON, rootObject)
+            .then(function (result) {
+                // return objJSON here, OR exportJSON(objJSON) here
+                this.exportService.exportJSON(objJSON, 
+                    {filename:  Date.now() + '.json'});
+                console.log(JSON.stringify(objJSON));
+            }.bind(this));
+        
+    };
+
+    // broken! only goes 1 layer deep
+    ExportAsJSONAction.prototype.writeChildren = function (struct, domainObject) {
+        if (domainObject.hasCapability('composition')) {
+            return domainObject.useCapability('composition').then(function (children) {
+                children.forEach(function (child) {
+                    struct[child.getId()] = child.getModel();
+                    console.log('child |' + child.getId() + '| ' + JSON.stringify(child.getModel()));
+                    this.writeChildren(struct, child);
+                }.bind(this))
+            }.bind(this));
+        } else {
+
+            return Promise.resolve();
+        }
     };
 
     return ExportAsJSONAction;
--- import-export/ImportAsJSONAction.js
+++ import-export/ImportAsJSONAction.js
@@ -12,10 +12,65 @@ define([], function () {
         "persisted":1499963825849
     };
 
+    var exampleJSON = {  
+       "root":{  
+          "composition":[  
+             "efafe9c0-f8a3-4efc-8e46-922efe8df678"
+          ],
+          "name":"foldy",
+          "type":"folder",
+          "modified":1499988633529,
+          "location":"mine",
+          "persisted":1499988633529
+       },
+       "efafe9c0-f8a3-4efc-8e46-922efe8df678":{  
+          "composition":[  
+             "efdf3323-c4d8-4d72-82bf-f6f0aa286dc2"
+          ],
+          "name":"nested foldy",
+          "type":"folder",
+          "modified":1499988658146,
+          "location":"affbdb4d-82cf-4faf-98c5-574f05af102a",
+          "persisted":1499988658146
+       },
+       "efdf3323-c4d8-4d72-82bf-f6f0aa286dc2":{  
+          "telemetry":{  
+             "values":[  
+                {  
+                   "name":"Time",
+                   "key":"utc",
+                   "format":"utc",
+                   "hints":{  
+                      "domain":1,
+                      "priority":0
+                   },
+                   "source":"utc"
+                },
+                {  
+                   "name":"Image",
+                   "key":"url",
+                   "format":"image",
+                   "hints":{  
+                      "image":1,
+                      "priority":1
+                   },
+                   "source":"url"
+                }
+             ]
+          },
+          "name":"slayer",
+          "type":"example.imagery",
+          "modified":1499988658143,
+          "location":"efafe9c0-f8a3-4efc-8e46-922efe8df678",
+          "persisted":1499988658143
+       }
+    };
+
     function ImportAsJSONAction(exportService, openmct, context) {
         this.exportService = exportService;
         this.context = context;
         this.openmct = openmct;
+        // parent should be defined here... lazy will do later
     }
 
     ImportAsJSONAction.prototype.perform = function() {
@@ -30,10 +85,74 @@ define([], function () {
                 clone.getCapability("persistence").persist();
                 parent.getCapability("persistence").persist();
             }.bind(this));
+
+        this.buildTree(exampleJSON);
     }; 
 
     ImportAsJSONAction.appliesTo = function (context) {
         return context.domainObject !== undefined;
+    };
+
+    ImportAsJSONAction.prototype.buildTree = function (struct) {
+        var parent = this.context.domainObject;
+        var objectToImport = parent.useCapability("instantiation", struct['root']);
+        // creates NEW domain object for the root object, type: example.getId()
+        parent.getCapability("composition")
+            .add(objectToImport) // this should happen last? not sure... lets      
+            .then(function (importedObject) {
+                //clone.getCapability("persistence").persist();
+                //parent.getCapability("persistence").persist();
+                
+                //this.appendChildren(importedObject, struct)
+                this.traverse(importedObject, struct);
+            }.bind(this));
+
+        // this.appendChildren(objectToImport, struct);
+        // last step here, persist 1st object
+
+        parent.getCapability("persistence").persist();
+        objectToImport.getCapability("persistence").persist();
+    };
+
+    ImportAsJSONAction.prototype.traverse = function (parent, struct) {
+        if (parent.hasCapability("composition")) {
+            parent.useCapability('composition').then(function (children) {
+                children.forEach(function (child, index) {
+                    console.log('reached: ' + JSON.stringify(child.getModel()));
+                });
+            });
+        }
+    };
+
+    ImportAsJSONAction.prototype.appendChildren = function (parent, struct) {
+        var newChild;
+        console.log(JSON.stringify(parent.getModel()));
+        if (parent.hasCapability("composition")) {
+            parent.useCapability('composition').then(function (children) {
+                children.forEach(function (child, index) {
+                    console.log('?: ' + child.getId());
+                    // here i need to get the NEW id and change parent.composition[index]
+                    // to the NEW id
+                    newChild = parent.useCapability("instantiation", struct[child.getId()]);
+                    children[index] = newChild.getId();
+                    
+                    parent.getCapability("composition")
+                        .add(newChild)
+                        .then(function (importedObject) {
+                            newChild.getCapability("persistence").persist();
+                        })
+                    //newChild.getCapability("persistence").persist();
+                    
+                });
+            });
+        }
+        // if (parent.hasCapability("composition")) {
+        //     parent.useCapability('composition').then(function (children) {
+        //         children.forEach(function (child) {
+        //             console.log('child!: ' + JSON.stringify(child.getModel()));
+        //         });
+        //     });
+        // }
     };
 
     return ImportAsJSONAction;
