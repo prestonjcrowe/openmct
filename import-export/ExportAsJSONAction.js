f9b1ab4e459f2f89c520da12d3f108a807fc6f27
Preston Crowe <prestonjcrowe@gmail.com>
2017-07-17 22:15:07 -0700

    [Import/Export] Initial import functionality

--- import-export/ExportAsJSONAction.js
+++ import-export/ExportAsJSONAction.js
@@ -23,31 +23,60 @@ define([], function () {
     ExportAsJSONAction.prototype.contructJSON = function (rootObject) {
         var objJSON = {};
         objJSON["root"] = rootObject.getModel();
-        this.writeChildren(objJSON, rootObject)
+        //var tree = this.writeChildren(objJSON, rootObject);
+        this.writeChildren(objJSON, rootObject, true)
             .then(function (result) {
+                this.exportService.exportJSON(objJSON, 
+                {filename:  Date.now() + '.json'});
+            }.bind(this))
+        
+      
+            //.then(function () {
+            //    console.log(JSON.stringify(tree));
+            //});
+        
+            /*.then(function (result) {
                 // return objJSON here, OR exportJSON(objJSON) here
                 this.exportService.exportJSON(objJSON, 
                     {filename:  Date.now() + '.json'});
                 console.log(JSON.stringify(objJSON));
-            }.bind(this));
+            }.bind(this)); */
         
     };
 
-    // broken! only goes 1 layer deep
-    ExportAsJSONAction.prototype.writeChildren = function (struct, domainObject) {
-        if (domainObject.hasCapability('composition')) {
-            return domainObject.useCapability('composition').then(function (children) {
-                children.forEach(function (child) {
-                    struct[child.getId()] = child.getModel();
-                    console.log('child |' + child.getId() + '| ' + JSON.stringify(child.getModel()));
-                    this.writeChildren(struct, child);
-                }.bind(this))
-            }.bind(this));
-        } else {
-
-            return Promise.resolve();
+    // THIS SHOULD RETURN STRUCT
+    ExportAsJSONAction.prototype.writeChildren = function (struct, domainObject, firstElem) {
+        if (firstElem === undefined) {
+            struct[domainObject.getId()] = domainObject.getModel();
+        //     // dont add root, root already added
         }
+        console.log('inside ' + JSON.stringify(struct));
+        if (domainObject.hasCapability('composition')) {
+            domainObject.useCapability('composition')
+                .then(function (children) {
+                    children.forEach(function (child) { 
+                        struct[child.getId()] = child.getModel();
+                        return this.writeChildren(struct, child);                        
+                    }.bind(this));
+            }.bind(this));
+        }
+        return Promise.resolve("leaf");
     };
+    
+    // ExportAsJSONAction.prototype.writeChildren = function (struct, domainObject) {
+    //     if (domainObject.hasCapability('composition')) {
+    //         return domainObject.useCapability('composition').then(function (children) {
+    //             children.forEach(function (child) {
+    //                 struct[child.getId()] = child.getModel();
+    //                 console.log('child |' + child.getId() + '| ' + JSON.stringify(child.getModel()));
+    //                 this.writeChildren(struct, child);
+    //             }.bind(this))
+    //         }.bind(this));
+    //     } else {
+    //             struct[domainObject.getId()] = domainObject.getModel();
+    //         //return Promise.resolve();
+    //     }
+    // };
 
     return ExportAsJSONAction;
 });
